e3a8250b69ad54b252bfd7cd86dfc78259e36b2f
john3825 <glassissue@yahoo.com>
2016-11-19 18:24:02 +0900

    link

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,15 @@
 console.log('This would be the main JS file.');
 <!--//
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:340586,hjsv:5};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:340586,hjsv:5};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+var input = document.getElementById("SiteID_Icon");
+iconSiteID.addEventListener( "click", (function(){
+window.open("https://john3825.github.io/internet-site-id-project/")
+}));
 
