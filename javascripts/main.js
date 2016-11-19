a37428e1247322320a663be1ecaf09a02c6d3d08
john3825 <glassissue@yahoo.com>
2016-11-19 18:31:34 +0900

    onload

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,6 @@
+window.onload = (function() {
 var iconSiteID = document.getElementById("SiteID_Icon");
 iconSiteID.addEventListener( "click", (function(){
 window.open("http://google.com")
 }));
+}
