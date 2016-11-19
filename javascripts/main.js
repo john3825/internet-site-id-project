76a9bc4836f0217f3f708c848ba0da021ee2b01d
john3825 <glassissue@yahoo.com>
2016-11-19 18:55:52 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,8 @@
-
+window.onload = (function() {
+ var iconSiteID = document.getElementById("SiteID_Icon");
+ iconSiteID.addEventListener("click",(function(){
+  window.open("https://john3825.github.io/internet-site-id-project/");
+ }));
+});
+                          
  
