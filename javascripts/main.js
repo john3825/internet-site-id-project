052fe2f145817a617527ba9778d0bfa582ec3769
john3825 <glassissue@yahoo.com>
2016-11-19 18:05:07 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,5 @@
 console.log('This would be the main JS file.');
-<!-- Hotjar Tracking Code for john3825.github.io/internet-site-id-project/ -->
-<script>
+<!--//
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:340586,hjsv:5};
@@ -9,4 +8,4 @@ console.log('This would be the main JS file.');
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
+
