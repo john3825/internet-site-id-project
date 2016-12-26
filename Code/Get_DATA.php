114be9b7ab7317b13ffc13c7984227838e0e0149
john3825 <glassissue@yahoo.com>
2016-12-26 11:33:37 +0900

    Code Sample Added

---
+++ Code/Get_DATA.php
@@ -0,0 +1,32 @@
+<?php
+	if ( @isset( $_POST["Internet_Site_ID"] ) ) {
+		$Internet_Site_ID = @json_decode( @base64_decode( $_POST["Internet_Site_ID"]));
+		if ( $Internet_Site_ID ) {
+			if ( $Internet_Site_ID -> Request == "GetDATA" ) {
+				echo Get_DATA::send();
+			}
+		}
+	} 
+	// Sample
+	final class Get_DATA {
+	
+		private static $Internet_Site_ID = ""; // ID number
+		
+		public function __construct() {
+		//
+		}
+
+		public function send() {
+			$ID_Data = "";
+			self::Get_DATA();
+			$ID_Data = $Internet_Site_ID;
+			return $ID_Data;
+		}
+		
+		private static final function Get_DATA() {
+			$ID_Data = "Internet_Site_ID_Number";
+			self::$Internet_Site_ID = $ID_Data;
+		}
+	}
+
+?>
